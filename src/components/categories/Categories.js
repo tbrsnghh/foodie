ad29a41db908d53d4f1ac94ea51415a7521509c0
Tbrsnghh <nhham1771@gmail.com>
2024-08-09 16:50:07 +0700

    Need to fix the food and drink categories

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -5,15 +5,32 @@ import { useSelector } from "react-redux";
 
 export default function Categories() {
   const { restaurants, food, drinks } = useSelector((state) => state.food);
-  //const categories = [...food,...drinks]
+  const categories = [...food, ...drinks];
+  console.log(categories);
   return (
-    <div>
-      
-      {food.length > 0 &&  
-        food.map((item, index) => (  
-          <h1 key={index}>{item}</h1>
-        ))}  
-    
-    </div>
+    <Row gutter={16}>
+      {categories.length > 0 &&
+        categories.map((item, index) => (
+          <Col span={8} key={index}>
+            <Card
+              hoverable
+              style={{
+                width: 260,
+                background: "none",
+                marginTop: 10,
+              }}
+              cover={
+                <img
+                  alt={item.name}
+                  src={item.img}
+                  style={{ height: 200, width: "100%", objectFit: "cover" }}
+                />
+              }
+            >
+              <Meta title={item.name} description=""/>
+            </Card>
+          </Col>
+        ))}
+    </Row>
   );
 }
