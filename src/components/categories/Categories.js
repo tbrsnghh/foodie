c6afb6806e7580f249c19114e582f0077407aa1c
Tbrsnghh <nhham1771@gmail.com>
2024-08-09 14:30:35 +0700

    nothing

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -8,30 +8,12 @@ export default function Categories() {
   //const categories = [...food,...drinks]
   return (
     <div>
-      <Row gutter={[16, 16]}> {/* Sử dụng Row với khoảng cách giữa các thẻ */}  
+      
       {food.length > 0 &&  
         food.map((item, index) => (  
-          <Col xs={24} sm={12} md={8} lg={6} key={index}> {/* Thiết lập số lượng thẻ trên mỗi hàng */}  
-            <Card  
-              hoverable  
-              style={{  
-                width: "100%", // Đảm bảo chiều rộng thẻ chiếm toàn bộ cột  
-                background: "none",  
-                marginTop: 10,  
-              }}  
-              cover={  
-                <img  
-                  style={{ height: 200, width: "100%", objectFit: "cover" }}  
-                  alt="example"  
-                  src="" // Đảm bảo sử dụng ảnh từ item  
-                />  
-              }  
-            >  
-              <Meta title={item} description={item.desc} /> {/* Cập nhật title và description */}  
-            </Card>  
-          </Col>  
+          <h1 key={index}>{item}</h1>
         ))}  
-    </Row>  
+    
     </div>
   );
 }
